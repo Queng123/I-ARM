aaf24028adf9b26c517629818eb2c5574173f477
queng <quentin.brejoin@gmail.com>
2024-06-01 14:22:01 +0200

    add twilio

---
+++ twilio-streams/index.js
@@ -0,0 +1,74 @@
+const WebSocket = require("ws");
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const wss = new WebSocket.Server({ server });
+
+//Include Google Speech to Text
+const speech = require("@google-cloud/speech");
+const client = new speech.SpeechClient();
+
+//Configure Transcription Request
+const request = {
+  config: {
+    encoding: "MULAW",
+    sampleRateHertz: 8000,
+    languageCode: "en-GB"
+  },
+  interimResults: true
+};
+
+// Handle Web Socket Connection
+wss.on("connection", function connection(ws) {
+console.log("New Connection Initiated");
+
+ let recognizeStream = null;
+
+  ws.on("message", function incoming(message) {
+    const msg = JSON.parse(message);
+    switch (msg.event) {
+      case "connected":
+        console.log(`A new call has connected.`);
+
+        // Create Stream to the Google Speech to Text API
+        recognizeStream = client
+          .streamingRecognize(request)
+          .on("error", console.error)
+          .on("data", data => {
+            console.log(data.results[0].alternatives[0].transcript);
+          });
+        break;
+      case "start":
+        console.log(`Starting Media Stream ${msg.streamSid}`);
+        break;
+      case "media":
+        // Write Media Packets to the recognize stream
+        recognizeStream.write(msg.media.payload);
+        break;
+      case "stop":
+        console.log(`Call Has Ended`);
+        recognizeStream.destroy();
+        break;
+    }
+  });
+});
+
+//Handle HTTP Request
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.post("/", (req, res) => {
+  res.set("Content-Type", "text/xml");
+
+  res.send(`
+    <Response>
+      <Start>
+        <Stream url="wss://${req.headers.host}/"/>
+      </Start>
+      <Say>I will stream the next 60 seconds of audio through your websocket</Say>
+      <Pause length="60" />
+    </Response>
+  `);
+});
+
+console.log("Listening at Port 8080");
+server.listen(8080);
